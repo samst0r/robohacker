f5d9a7dd30e1e4eee3f337bd5a822c2a702e502e
Sam Ward <sams@live.co.uk>
2013-03-01 11:42:47 +0000

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Robohacker::Application.config.secret_token = 'd96d2a16a0fdd8ad36eaa8d2760a57f1021fbab8de337c453d740e84f2c7028ca6ae32fc6c8b876418acdd6ce5eb8de1fef71a43cbc473e4e8825261830db86f'
